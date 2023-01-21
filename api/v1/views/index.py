c5ca6f2306e03fb67876fc51512c72c46a0abb93
MercyKorir <mcthegreat78@gmail.com>
2023-01-21 16:02:57 +0300

    I have added stats route

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -2,8 +2,17 @@
 """returns JSON status ok"""
 from flask import jsonify, request
 from api.v1.views import app_views
+from models import storage
 
 @app_views.route("/status", methods=['GET'])
 def status():
     if request.method == 'GET':
-        return jsonify({"status": "OK"}) 
+        return jsonify({"status": "OK"})
+
+@app_views.route('/stats', methods=['GET'])
+def stats():
+    """
+    function to return the count of all class objects
+    """
+    if request.method == 'GET':
+        return jsonify({cls.__name__: storage.count(cls) for cls in storage.all().values()})
