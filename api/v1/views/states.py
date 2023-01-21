57616a1fad03a4d206fb568206797ae9127b3c0c
MercyKorir <mcthegreat78@gmail.com>
2023-01-21 18:25:05 +0300

    I have added the states

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -1,3 +1,59 @@
 #!/usr/bin/python3
 """Handles all default RESTful API actions"""
+from models import storage
+from models.state import State
+from flask import jsonify, request
+from api.v1.views import app_views
 
+
+@app_views.route('/states', methods=['GET'])
+def get_states():
+    states = storage.all(State)
+    states = [state.to_dict() for state in states.values()]
+    return jsonify(states)
+
+
+@app_views.route('/states/<state_id>', methods=['GET'])
+def get_state(state_id):
+    state = storage.get(State, state_id)
+    if state is None:
+        return jsonify({'error': 'Not found'}), 404
+    return jsonify(state.to_dict())
+
+
+@app_views.route('/states/<state_id>', methods=['DELETE'])
+def delete_state(state_id):
+    state = storage.get(State, state_id)
+    if state is None:
+        return jsonify({'error': 'Not found'}), 404
+    storage.delete(state)
+    storage.save()
+    return jsonify({}), 200
+
+
+@app_views.route('/states', methods=['POST'])
+def create_state():
+    data = request.get_json()
+    if data is None:
+        return jsonify({'error': 'Not a JSON'}), 400
+    if 'name' not in data:
+        return jsonify({'error': 'Missing name'}), 400
+    state = State(**data)
+    storage.new(state)
+    storage.save()
+    return jsonify(state.to_dict()), 201
+
+
+@app_views.route('/states/<state_id>', methods=['PUT'])
+def update_state(state_id):
+    state = storage.get(State, state_id)
+    if state is None:
+        return jsonify({'error': 'Not found'}), 404
+    data = request.get_json()
+    if data is None:
+        return jsonify({'error': 'Not a JSON'}), 400
+    for key, value in data.items():
+        if key not in ['id', 'created_at', 'updated_at']:
+            setattr(state, key, value)
+    storage.save()
+    return jsonify(state.to_dict()), 200
