084cbb24d1788bc23100a08fa2d75e384be566b6
MercyKorir <mcthegreat78@gmail.com>
2023-01-21 16:20:39 +0300

    I have fixed some documentation errors

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -4,10 +4,12 @@ from flask import jsonify, request
 from api.v1.views import app_views
 from models import storage
 
+
 @app_views.route("/status", methods=['GET'])
 def status():
     if request.method == 'GET':
         return jsonify({"status": "OK"})
+
 
 @app_views.route('/stats', methods=['GET'])
 def stats():
@@ -15,4 +17,5 @@ def stats():
     function to return the count of all class objects
     """
     if request.method == 'GET':
-        return jsonify({cls.__name__: storage.count(cls) for cls in storage.all().values()})
+        return jsonify({cls.__name__: storage.count(cls)
+                        for cls in storage.all().values()})
